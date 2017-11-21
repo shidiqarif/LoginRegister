eddfe7c5a2f2d1f461fa70cbdfb02f0c87d47975
shidiqarifs <shidiqarifs@gmail.com>
2017-11-21 15:59:18 +0700

    Initial commit

---
+++ app/src/main/java/com/shidiqarifs/loginregister/activities/UsersListActivity.java
@@ -0,0 +1,97 @@
+package com.shidiqarifs.loginregister.activities;
+
+/**
+ * Created by shidiqarifs on 17/11/2017.
+ */
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+
+import com.shidiqarifs.loginregister.R;
+import com.shidiqarifs.loginregister.adapters.UsersRecyclerAdapter;
+import com.shidiqarifs.loginregister.model.User;
+import com.shidiqarifs.loginregister.sql.DatabaseHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsersListActivity extends AppCompatActivity {
+
+    private AppCompatActivity activity = UsersListActivity.this;
+    private AppCompatTextView textViewName;
+    private RecyclerView recyclerViewUsers;
+    private List<User> listUsers;
+    private UsersRecyclerAdapter usersRecyclerAdapter;
+    private DatabaseHelper databaseHelper;
+    Button btnLogout;
+    DatabaseHelper MyDB;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users_list);
+        getSupportActionBar().setTitle("");
+        initViews();
+        initObjects();
+        MyDB = new DatabaseHelper(this);
+        btnLogout = (Button) findViewById(R.id.appCompatButtonLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                Intent intent = new Intent(UsersListActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });}
+
+
+    private void initViews() {
+        textViewName = (AppCompatTextView) findViewById(R.id.textViewName);
+        recyclerViewUsers = (RecyclerView) findViewById(R.id.recyclerViewUsers);
+    }
+
+    private void initObjects() {
+        listUsers = new ArrayList<>();
+        usersRecyclerAdapter = new UsersRecyclerAdapter(listUsers);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerViewUsers.setLayoutManager(mLayoutManager);
+        recyclerViewUsers.setItemAnimator(new DefaultItemAnimator());
+        recyclerViewUsers.setHasFixedSize(true);
+        recyclerViewUsers.setAdapter(usersRecyclerAdapter);
+        databaseHelper = new DatabaseHelper(activity);
+
+        String emailFromIntent = getIntent().getStringExtra("EMAIL");
+        textViewName.setText(emailFromIntent);
+
+        getDataFromSQLite();
+    }
+
+    private void getDataFromSQLite() {
+        // AsyncTask is used that SQLite operation not blocks the UI Thread.
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                listUsers.clear();
+                listUsers.addAll(databaseHelper.getAllUser());
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                usersRecyclerAdapter.notifyDataSetChanged();
+            }
+        }.execute();
+    }
+}
